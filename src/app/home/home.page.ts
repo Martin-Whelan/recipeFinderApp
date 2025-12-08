88e20ba8eba16d542b7f67a93590456abdfaed94
Martin-Whelan <G00438844@atu.ie>
2025-12-08 22:40:33 +0000

    Added pages and started header component, added icon buttons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {heart, settings} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton, IonButtons],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router: Router) {
+    addIcons({heart, settings});
+  }
+
+  goToFavourites(){
+    this.router.navigate(['/favourites']);
+  }
+
+  goToSettings(){
+    this.router.navigate(['/settings'])
+  }
 }
