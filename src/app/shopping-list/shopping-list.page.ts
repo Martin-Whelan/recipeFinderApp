c71706d0c771480814bd5933a57be2a41e08af10
Martin-Whelan <G00438844@atu.ie>
2025-12-10 22:24:53 +0000

    fix bug with router link of shopping list page

--- src/app/shopping-list/shopping-list.page.ts
+++ src/app/shopping-list/shopping-list.page.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { Storage } from '@ionic/storage-angular';
 import { IonContent, IonButtons, IonIcon, IonButton, IonCheckbox, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { AppHeaderComponent } from "../components/app-header/app-header.component";
 import { addIcons } from 'ionicons';
 import { arrowBack, trash } from 'ionicons/icons';
 
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.page.html',
   styleUrls: ['./shopping-list.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonItem, IonButton, IonIcon, IonButtons, IonContent, CommonModule, FormsModule, AppHeaderComponent, IonCheckbox]
+  imports: [ RouterLink, IonLabel, IonItem, IonButton, IonIcon, IonButtons, IonContent, CommonModule, FormsModule, AppHeaderComponent, IonCheckbox]
 })
 export class ShoppingListPage implements OnInit {
   shoppingList: any[] = [];
 
   constructor(
-    private storage: Storage
+    private storage: Storage,
     ) 
     { 
       addIcons({ arrowBack, trash });
